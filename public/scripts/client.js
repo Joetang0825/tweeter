9b6a209260cfa99fbde5f67dccd183395144cfe6
Joe Tang <joetang81@gmail.com>
2022-03-24 23:03:45 -0400

    Fetching tweets with Ajax + add timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 
 $(document).ready(function () {
   const createTweetElement = function (tweet) {
+    time = timeago.format(tweet.created_at);
     let $tweet =
       `<article class="tweetContainer">
           <header class="tweetHeader">
@@ -46,7 +22,7 @@ $(document).ready(function () {
             <span>${tweet.content.text}</span>
           </div>
           <footer class="tweetFooter">
-            <span>${tweet.created_at}</span>
+            <span>${time}</span>
             <div class="tweetIcons">
               <i class="fa-solid fa-flag"></i>
               <i class="fa-solid fa-retweet"></i>
@@ -68,7 +44,6 @@ $(document).ready(function () {
     });
   }
 
-  renderTweets(data);
 
 
   $("form").submit(function (event) {
